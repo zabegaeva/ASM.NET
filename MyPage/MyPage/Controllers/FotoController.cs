1262d8d108b91353cb7fca17ba5d90eae3ee7284
zabegaeva <zabegaeva13@gmail.com>
2016-02-29 22:13:56 +0300

    Added files via upload

---
+++ MyPage/MyPage/Controllers/FotoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPage.Controllers
+{
+    public class FotoController : Controller
+    {
+        // GET: Foto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyPage/MyPage/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowContact()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
